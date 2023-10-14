645d5d254fcfe9db5562e43170fc35f1bddc94b1
Mahmoud <mahmoud.talass@gmail.com>
2023-10-14 17:27:05 -0400

    Create first test for Gameboard to check weather ship was placed

---
+++ src/scripts/model/model-tests/Gameboard.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+describe("Gameboard tests", () => {
+    let gameBoard;
+    beforeEach(()=> {
+        gameBoard = new Gameboard();
+    })
+    test("Place ship on board", ()=> {
+        expect
+    })
+})
