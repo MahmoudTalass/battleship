3bbd7eff49145b2a8b358b61501eefa3df5d1277
Mahmoud <mahmoud.talass@gmail.com>
2023-10-15 17:48:17 -0400

    Create test for receiveAttack method for Gameboard

--- src/scripts/model/model-tests/Gameboard.test.js
+++ src/scripts/model/model-tests/Gameboard.test.js
@@ -14,4 +14,11 @@ describe("Gameboard tests", () => {
       expect(board.grid[7][4]).toBeFalsy;
       expect(board.grid[8][4]).toBeFalsy;
    });
+
+   test("Ship received hit", () => {
+      board.placeShip(new Ship(5), "vertical", 4, 4);
+      board.receiveAttack(4, 5);
+
+      expect(board.grid[4][5].numOfHits).toBe(1);
+   });
 });
