6d4091c9f34ad43175def681d41db4762c410ea1
Mahmoud <mahmoud.talass@gmail.com>
2023-10-15 13:01:55 -0400

    Add devServer stuff to run app on localhost:8080

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
 
 module.exports = {
+   mode: "development",
    entry: "./src/index.js",
    output: {
       filename: "main.js",
       path: path.resolve(__dirname, "dist"),
+   },
+   devtool: "inline-source-map",
+   devServer: {
+      static: "./dist",
    },
    module: {
       rules: [
@@ -17,10 +22,5 @@ module.exports = {
             type: "asset/resource",
          },
       ],
-      moduleNameMapper: {
-         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-            "<rootDir>/__mocks__/fileMock.js",
-         "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
-      },
    },
 };
