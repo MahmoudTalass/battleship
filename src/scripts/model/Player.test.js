a2e93325ae2981a481817c00b4ec02620f75b475
Mahmoud <mahmoud.talass@gmail.com>
2023-10-28 13:28:29 -0400

    Adjust recieved attack test and test switching turns

--- src/scripts/model/Player.test.js
+++ src/scripts/model/Player.test.js
@@ -14,12 +14,19 @@ describe("Player tests", () => {
       expect(player.isPlayerTurn()).toBe(true);
    });
 
+   test("Should be enemy turn", () => {
+      player.switchPlayerTurns();
+      player.switchPlayerTurns();
+
+      expect(player.isPlayerTurn()).toBe(false);
+   });
+
    test("Player attack received", () => {
       const enemyBoard = new Gameboard();
       const ship = new Ship(3);
       enemyBoard.placeShip(ship, "vertical", 4, 5);
 
-      expect(player.attack(enemyBoard, 6, 5)).toBe("received attack");
+      expect(player.attack(enemyBoard, 6, 5)).toBe("received");
       expect(ship.numOfHits).toBe(1);
       expect(enemyBoard.grid[6][5]).toBe("X");
    });
