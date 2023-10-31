b0b06902c552f4c37e5f4abd1535620ebb42e33e
Mahmoud <mahmoud.talass@gmail.com>
2023-10-31 09:17:58 -0400

    Return the successful attack message when random attack isn't illegal

--- src/scripts/model/ComputerPlayer.js
+++ src/scripts/model/ComputerPlayer.js
@@ -10,6 +10,7 @@ export default class ComputerPlayer {
             continue;
          } else {
             isSuccessfulAttackMade = true;
+            return attackMessage;
          }
       }
    }
