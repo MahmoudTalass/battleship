fb043b589822174045427ded53cec940be0f8b46
Mahmoud <mahmoud.talass@gmail.com>
2023-11-04 14:44:55 -0400

    Create player and enemy game board containers

--- src/scripts/view/render-game.js
+++ src/scripts/view/render-game.js
@@ -5,6 +5,41 @@ function renderGameScreen() {
    gameScreenContainer.classList.add("game-screen-container");
    gameScreenContainer.id = "game-screen-container";
 
+   const playerBoardContainer = document.createElement("div");
+   playerBoardContainer.classList.add("player-board-container");
+   playerBoardContainer.id = "player-board-container";
+
+   const playerBoard = document.createElement("div");
+   playerBoard.classList.add("player-board", "board");
+   playerBoard.id = "player-board";
+
+   const playerNameTag = document.createElement("p");
+   playerNameTag.classList.add("player-nametag");
+   playerNameTag.id = "player-nametag";
+   playerNameTag.textContent = "You";
+
+   playerBoardContainer.appendChild(playerNameTag);
+   playerBoardContainer.appendChild(playerBoard);
+
+   const enemyBoardContainer = document.createElement("div");
+   enemyBoardContainer.classList.add("enemy-board-container");
+   enemyBoardContainer.id = "enemy-board-container";
+
+   const enemyBoard = document.createElement("div");
+   enemyBoard.classList.add("enemy-board", "board");
+   enemyBoard.id = "enemy-board";
+
+   const enemyNameTag = document.createElement("p");
+   enemyNameTag.classList.add("enemy-nametag");
+   enemyNameTag.id = "enemy-nametag";
+   enemyNameTag.textContent = "Enemy";
+
+   enemyBoardContainer.appendChild(enemyNameTag);
+   enemyBoardContainer.appendChild(enemyBoard);
+
+   gameScreenContainer.appendChild(playerBoardContainer);
+   gameScreenContainer.appendChild(enemyBoardContainer);
+
    mainScreenContainer.appendChild(gameScreenContainer);
 }
 
