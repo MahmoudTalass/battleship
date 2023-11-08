cdf065aaef635533ae1afc041267b89fe5ee2704
Mahmoud <mahmoud.talass@gmail.com>
2023-11-08 10:39:55 -0500

    Remove code as it is not appropriate to be in the gameloop

--- src/scripts/controller/GameLoop.js
+++ src/scripts/controller/GameLoop.js
@@ -4,21 +4,4 @@ import RandomOperations from "../model/RandomOperations";
 import { renderGameScreen } from "../view/render-game";
 import { showGameboard } from "./GameController";
 
-export default function GameLoop() {
-   renderGameScreen();
-
-   const playerBoardElement = document.getElementById("player-board");
-   const enemyBoardElement = document.getElementById("enemy-board");
-
-   const player = new Player();
-   const playerBoard = new Gameboard();
-
-   const enemy = new Player();
-   const enemyBoard = new Gameboard();
-
-   RandomOperations.placeAllShipsRandomly(playerBoard);
-   RandomOperations.placeAllShipsRandomly(enemyBoard);
-
-   showGameboard(playerBoardElement, playerBoard.grid);
-   showGameboard(enemyBoardElement, enemyBoard.grid);
-}
+export default function GameLoop() {}
