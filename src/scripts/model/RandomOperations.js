fc84bf7252632581a6b35ee2814c10a625cb1b70
Mahmoud <mahmoud.talass@gmail.com>
2023-11-11 15:02:20 -0500

    Make randomAttack return attack status, and coordinates of attack

--- src/scripts/model/RandomOperations.js
+++ src/scripts/model/RandomOperations.js
@@ -12,7 +12,7 @@ export default class RandomOperations {
             continue;
          } else {
             isSuccessfulAttackMade = true;
-            return attackMessage;
+            return { attackMessage, x, y };
          }
       }
    }
