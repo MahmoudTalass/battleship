2bb0768c1e54929d2f2818b6526377b433f535ff
Mahmoud <mahmoud.talass@gmail.com>
2023-11-12 19:37:20 -0500

    Create a game loop that makes changes as attacks happen
    Changes to the board, and player turns

--- src/scripts/controller/GameLoop.js
+++ src/scripts/controller/GameLoop.js
@@ -1,7 +1,20 @@
-import Gameboard from "../model/Gamebaord";
-import Player from "../model/Player";
-import RandomOperations from "../model/RandomOperations";
-import { renderGameScreen } from "../view/render-game";
-import { showGameboard } from "./GameController";
+import { markMissedAttack, markSuccessfulAttack } from "../view/renderGame";
+import { player, enemy, enemyAttack } from "./GameController";
 
-export default function GameLoop() {}
+export default function GameLoop(x, y) {
+   if (player.isPlayerTurn) {
+      let attackStatus = player.attack(enemy.board, x, y);
+
+      if (attackStatus == "illegal") {
+         return;
+      } else if (attackStatus == "missed") {
+         markMissedAttack(x, y);
+      } else if (attackStatus == "received") {
+         markSuccessfulAttack(x, y);
+      }
+      player.switchPlayerTurns();
+      enemyAttack();
+   } else {
+      return;
+   }
+}
