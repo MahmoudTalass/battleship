44dc8b28faa74756d65ccfccc8c12dfc5e3b5641
Mahmoud <mahmoud.talass@gmail.com>
2023-12-17 19:02:47 -0500

    Make proper calls to marking function and make turn switching function

--- src/scripts/controller/GameController.js
+++ src/scripts/controller/GameController.js
@@ -6,13 +6,14 @@ import {
 } from "../view/renderGame";
 import Player from "../model/Player";
 import RandomOperations from "../model/RandomOperations";
+import { loadEventListeners } from "../view/eventListeners";
 
 let player = null;
 let enemy = null;
 
 function showGameboard(playerBoardElement, playerGrid) {
-   for (let x = 0; x < 10; x++) {
-      for (let y = 0; y < 10; y++) {
+   for (let y = 0; y < 10; y++) {
+      for (let x = 0; x < 10; x++) {
          const square = renderGridSquare(x, y);
          if (playerGrid[x][y] === null) {
             square.classList.add("water");
@@ -36,23 +37,45 @@ function initiateGame() {
    player = new Player();
    enemy = new Player();
 
+   RandomOperations.randomizeTurns(player, enemy);
+
    RandomOperations.placeAllShipsRandomly(player.board);
    RandomOperations.placeAllShipsRandomly(enemy.board);
 
    showGameboard(playerBoardElement, player.board.grid);
    showGameboard(enemyBoardElement, enemy.board.grid);
+
+   if (enemy.isPlayerTurn()) {
+      enemyAttack();
+   }
+   loadEventListeners();
 }
 
 function enemyAttack() {
    const randomAttackInfo = RandomOperations.randomAttack(enemy, player.board);
 
-   if (randomAttackInfo.attackStatus == "missed") {
-      markMissedAttack(randomAttackInfo.x, randomAttackInfo.y);
+   if (randomAttackInfo.attackMessage == "missed") {
+      markMissedAttack(randomAttackInfo.x, randomAttackInfo.y, "playerBoard");
    } else {
-      markSuccessfulAttack(randomAttackInfo.x, randomAttackInfo.y);
+      markSuccessfulAttack(
+         randomAttackInfo.x,
+         randomAttackInfo.y,
+         "playerBoard"
+      );
    }
-
-   enemy.switchPlayerTurns();
+   switchPlayerTurns();
 }
 
-export { showGameboard, initiateGame, player, enemy, enemyAttack };
+function switchPlayerTurns() {
+   player.switchTurn();
+   enemy.switchTurn();
+}
+
+export {
+   showGameboard,
+   initiateGame,
+   player,
+   enemy,
+   enemyAttack,
+   switchPlayerTurns,
+};
